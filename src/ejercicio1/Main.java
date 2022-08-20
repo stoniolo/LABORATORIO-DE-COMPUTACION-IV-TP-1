1ce094f949c15ad83172e1bd637b79c929164d4c
Paul-2000 <55331378+Paul-2000@users.noreply.github.com>
2022-08-20 11:04:57 -0300

    Punto 8

--- src/ejercicio1/Main.java
+++ src/ejercicio1/Main.java
@@ -12,6 +12,16 @@ public class Main {
 		
 		System.out.println(empleado1.toString());
 		
+		Empleado[] vEmpleados = new Empleado[3];
+		
+		vEmpleados[0] = new Empleado("Jose", 30);
+		vEmpleados[1] = new Empleado("Pedro", 35);
+		vEmpleados[2] = new Empleado("Alejandro", 40);
+		
+		for (Empleado empleado2 : vEmpleados) {
+			System.out.println(empleado2.toString());
+		}
+		
 		System.out.println(Empleado.devuelveProximoID());
 	}
 }
